cfb3e1caf28c02ab817891a4156d4b81517244f7
Brian Ellis <brian@Brians-MacBook-Pro.local>
2019-01-04 23:22:12 -0600

    now modularized

--- go.sh
+++ go.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #find out/ -name "*" -print0 | xargs -0 rm
 rm out/*
-clingo 10 notes.lp prettify.py 
+clingo 10 generator.lp sp-general.lp sp-1.lp cf/c-major-1.lp prettify.py 
 cd out
-lilypond *
+lilypond * > /dev/null 2>&1 &
+sleep 1.5 
+open *.pdf
 
