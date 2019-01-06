5d84767bc086f3fc265c520fc07b0b875b522af7
Brian Ellis <brian@Brians-MacBook-Pro.local>
2019-01-05 22:17:24 -0600

    moved for modularity

--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-clingo 10 generator.lp sp-general.lp sp-1.lp cf/c-major-1.lp prettify.py  --parallel-mode 4
+clingo 10 generator.lp sp-general.lp sp-1.lp sp-cantus/c-major-1.lp prettify.py  --parallel-mode 4
