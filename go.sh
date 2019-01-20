25b0385f366534f0a60516e5cd1569618ceebda4
Brian Ellis <brian@Brians-MacBook-Pro.local>
2019-01-20 01:28:55 -0600

    now with guitar chord 2

--- go.sh
+++ go.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-clingo 10 generator.lp sp-general.lp sp-1.lp sp-cantus/c-major-1.lp prettify.py  --parallel-mode 4
+clingo 15 generator.lp chords-2.lp prettify.py chords-general.lp 
+# clingo 10 generator.lp sp-general.lp sp-1.lp sp-cantus/c-major-1.lp prettify.py  --parallel-mode 4
